c365d819dea37350b8b04c4f6367f6df603bcc42
whatever193 <103187849+whatever193@users.noreply.github.com>
2022-09-25 00:57:40 +0700

    Update browserstack_sample.py

--- android/browserstack_sample.py
+++ android/browserstack_sample.py
@@ -3,45 +3,33 @@ from appium.webdriver.common.mobileby import MobileBy
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
-
+  
 desired_cap = {
     # Set your access credentials
-    "browserstack.user" : "YOUR_USERNAME",
-    "browserstack.key" : "YOUR_ACCESS_KEY",
-
+    "browserstack.user" : "usapaincraft_KdGprt",
+    "browserstack.key" : "TTT9o1kBsqYpsUvrVqcB",
+  
     # Set URL of the application under test
-    "app" : "bs://<app-id>",
-
+    "app" : "bs://0d108a48232e1ea2ef6fc1ac4ca707e36356d1fb",
+  
     # Specify device and os_version for testing
-    "device" : "Google Pixel 3",
-    "os_version" : "9.0",
-    
+    "device" : "iPhone 12 Pro Max",
+    "os_version" : "16",
+      
     # Set other BrowserStack capabilities
     "project" : "First Python project", 
     "build" : "browserstack-build-1",
     "name" : "first_test"
 }
-
+  
 # Initialize the remote Webdriver using BrowserStack remote URL
 # and desired capabilities defined above
 driver = webdriver.Remote(
     command_executor="http://hub-cloud.browserstack.com/wd/hub", 
     desired_capabilities=desired_cap
 )
-
-# Test case for the BrowserStack sample Android app. 
-# If you have uploaded your app, update the test case here. 
-search_element = WebDriverWait(driver, 30).until(
-    EC.element_to_be_clickable((MobileBy.ACCESSIBILITY_ID, "Search Wikipedia"))
-)
-search_element.click()
-search_input = WebDriverWait(driver, 30).until(
-    EC.element_to_be_clickable((MobileBy.ID, "org.wikipedia.alpha:id/search_src_text"))
-)
-search_input.send_keys("BrowserStack")
-time.sleep(5)
-search_results = driver.find_elements_by_class_name("android.widget.TextView")
-assert(len(search_results) > 0)
-
+  
+# If you have uploaded your app, write your test case here. 
+  
 # Invoke driver.quit() after the test is done to indicate that the test is completed.
 driver.quit()
